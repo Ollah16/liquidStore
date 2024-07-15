029232d85d72cea6ea12451c7b741396940b1d9b
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-07-15 18:20:41 -0400

    reviewIV

--- src/components/main/WelcomeProps.jsx
+++ src/components/main/WelcomeProps.jsx
@@ -1,36 +1,39 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Button from '../Button'
-import image from '../../assets/lloyds_homepage_cl_hero_mobile_final.jpg';
-// import imageTwo from '../../assets/lloyds_homepage_cl_hero_mobile_final.jpg';
+import imageDesktop from '../../assets/lloyds_homepage_cl_hero_mobile_final.jpg';
+import imageMobile from '../../assets/lloyds-homepage-strategic-engagement.jpg';
 import WelcomeDocs from '../../constants/WelcomeDocs';
-import PaginatorControl from '../paginate/PaginatorControl';
 
 const WelcomeProps = () => {
 
     const welcomeDoc = WelcomeDocs()
     const welcomeRef = useRef([])
-    const imgRef = useRef()
+    const imageDesktopRef = useRef()
+    const imageMobileRef = useRef()
     const imgParentRef = useRef()
     const adRef = useRef()
 
     useEffect(() => {
-        const refImage = imgRef.current;
+        const deskTopImgRef = imageDesktopRef.current;
+        const mobileImageRef = imageMobileRef.current
         const advertRef = adRef.current;
         const repParentRef = imgParentRef.current
 
         const calculateHeight = () => {
-            const advertHeight = parseFloat(getComputedStyle(advertRef).height);
-            const imageHeight = parseFloat(getComputedStyle(refImage).height);
+            const advertHeight = advertRef.offsetHeight
+            const imageHeight = window.innerWidth > 768 ? deskTopImgRef.offsetHeight : mobileImageRef.offsetHeight
             return advertHeight + imageHeight;
         };
 
+
         const handleSize = () => {
+
             if (window.innerWidth < 768) {
-                advertRef.style.height = `100%`;
+                advertRef.style.height = ``;
                 repParentRef.style.height = `${calculateHeight()}px`;
             } else {
-                repParentRef.style.height = `${refImage.offsetHeight}px`;
-                advertRef.style.height = `${refImage.offsetHeight}px`;
+                repParentRef.style.height = `${deskTopImgRef.offsetHeight}px`;
+                advertRef.style.height = `${deskTopImgRef.offsetHeight}px`;
             }
         };
 
@@ -40,30 +43,43 @@ const WelcomeProps = () => {
         return () => {
             window.removeEventListener('resize', handleSize);
         };
-    }, [imgRef, imgParentRef, adRef]);
+    }, [imageDesktopRef, imgParentRef, adRef]);
 
 
     return (
         <div
-            className="relative w-full flex after:content-[''] after:h-full after:absolute after:w-full after:inline-block after:brightness-75 overflow-hidden"
+            className="relative w-full flex after:content-[''] after:h-full after:absolute after:w-full after:inline-block after:opacity-70 overflow-hidden"
             ref={imgParentRef}>
             {welcomeDoc.slice(0, 1).map((doc, index) => (
                 <div
-                    ref={(el) => (welcomeRef.current[index] = el)}
+                    ref={(el) => welcomeRef.current[index] = el}
                     key={index}
-                    className={`absolute w-full h-fit z-20 top-0 left-0 right-0`}
+                    className={`w-full relative z-20 top-0 left-0 right-0`}
                 >
-                    <div className='h-auto relative md:absolute'>
+                    <div className='hidden md:inline-block h-auto absolute'>
                         <img
-                            ref={imgRef}
-                            src={image}
+                            ref={imageDesktopRef}
+                            src={imageDesktop}
+                            // height='411'
+                            // width='2000'
+                            alt="Welcome to Lloyds"
+                            className="object-cover w-full h-full"
+                        />
+                    </div>
+
+                    <div className='md:hidden block h-auto'>
+                        <img
+                            src={imageMobile}
+                            ref={imageMobileRef}
+                            height='411'
+                            width='2000'
                             alt="Welcome to Lloyds"
                             className="object-cover w-full h-auto"
                         />
                     </div>
 
-                    <div ref={adRef} className="p-5 mb-3 h-full md:max-w-[62%] lg:max-w-[53%] flex items-center relative bg-theme md:bg-transparent">
-                        <div className='my-auto'>
+                    <div ref={adRef} className="p-5 mb-3 h-fit md:max-w-[62%] lg:max-w-[53%] flex items-center relative bg-theme md:bg-transparent">
+                        <div className='md:my-auto'>
                             <div className='mb-5'>
                                 <p className="text-white font-semibold text-xl md:text-2xl lg:text-4xl mb-3">{doc.title}</p>
                                 <p className="text-white font-semibold text-base md:text-base lg:text-lg">{doc.subTitle}</p>
