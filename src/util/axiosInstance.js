25f762770af8cc751a82632f705d4f45c83357d6
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-08-02 02:02:48 -0500

    serverI

--- src/util/axiosInstance.js
+++ src/util/axiosInstance.js
@@ -21,15 +21,24 @@ axiosInstance.interceptors.request.use((config) => {
 });
 
 // Response interceptor to handle errors
-axiosInstance.interceptors.response.use((response) => {
-    return response;
-}, (error) => {
-    if (error.response && error.response.status === 401) {
-        // Handle token expiration
+axiosInstance.interceptors.response.use(
+    (response) => {
+        // Return the response as is if successful
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            // Handle token expiration or invalid token scenarios
+            const errorMessage = error.response.data.message || '';
 
-        // localStorage.removeItem('token');
+            if (errorMessage.includes('Token expired') || errorMessage.includes('Invalid token')) {
+                // Remove the token from localStorage if it has expired or is invalid
+                localStorage.removeItem('token');
+            }
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-});
+);
+
 
 export default axiosInstance;
