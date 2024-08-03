9ad7de39392b96ffb105af3102484c989c3397eb
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-08-03 16:06:58 -0500

    balance

--- src/components/StatementComponent/Balance.jsx
+++ src/components/StatementComponent/Balance.jsx
@@ -24,7 +24,7 @@ const Balance = () => {
     return (
         <div className='bg-gray-100 p-5'>
             <p className='mb-3 flex gap-x-3 items-baseline leading-none'><span className='font-semibold text-lg tracking-tight inline-block lg:w-1/12'>{accountType}</span><span>{sortCode}</span> <span>{accountNumber}</span></p>
-            <p className='flex items-baseline gap-x-3'><span className='font-semibold text-lg tracking-tight flex items-center'>£{balance.toLocaleString()}</span><span className='font-light text-sm pl-5'>Balance after pending</span> </p>
+            <p className='flex items-baseline gap-x-3'><span className='font-semibold text-base md:text-lg tracking-tight flex items-center'>£{balance.toLocaleString()}</span><span className='font-light text-xs md:text-sm pl-5'>Balance after pending</span> </p>
         </div>
     )
 }
