f32b800420c63ab37fb02eb0a081e49049f28091
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-08-06 13:53:35 -0500

    mobile view

--- src/components/StatementComponent/BankStatementMobile.jsx
+++ src/components/StatementComponent/BankStatementMobile.jsx
@@ -35,8 +35,8 @@ const BankStatementMobile = () => {
                                                     </span>
                                                 </div>
                                             </td>
-                                            <td className='max-w-[200px] px-2 overflow-hidden max-h-[46px]'>
-                                                <h3 className='text-sm inline-block h-[46px] sm:text-base w-11/12 sm:w-full text-nowrap text-ellipsis overflow-hidden'>{state.transaction_description}</h3>
+                                            <td className='w-5/12 px-2 overflow-hidden max-h-[46px]'>
+                                                <h3 className='text-sm inline-block max-w-[200px] sm:max-w-full sm:text-base w-11/12 sm:w-full text-nowrap text-ellipsis overflow-hidden'>{state.transaction_description}</h3>
                                             </td>
                                             <td className='w-1/2 sm:w-2/12 px-2'>
                                                 <div className='flex flex-col items-end w-full'>
