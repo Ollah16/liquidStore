40aa3a96251e1c91325586ddab4284de45d91521
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-08-27 08:45:51 -0500

    review

--- src/components/AccountComponent/ExpandNav.jsx
+++ src/components/AccountComponent/ExpandNav.jsx
@@ -18,7 +18,7 @@ const ExpandNav = () => {
     const contactClass = `justify-between hover:bg-white/5 transition-colors ease-in-out duration-200 items-center after:content-[''] after:bg-theme_light after:w-full after:h-[.5px] after:left-0 after:absolute after:bottom-0 relative`
 
     return (
-        <nav className={`${isExpanded ? 'translate-x-0' : 'translate-x-[100%]'} right-0 md:hidden w-full bg-theme top-[94px] min-h-screen z-50 h-full transition-all ease-in-out duration-300 fixed bottom-0 my-0`}>
+        <nav className={`${isExpanded ? 'translate-x-0' : 'translate-x-[100%]'} right-0 md:hidden w-full bg-theme top-[94px] min-h-screen z-50 h-full transition-translate ease-in-out duration-300 fixed bottom-0 my-0`}>
             <div className={`before:h-[1px] before:w-full before:absolute after:h-[150px] after:content-[''] after:block after:w-full after:relative before:top-0 before:bg-theme_light overflow-y-auto max-h-screen`}>
                 <MobileSetting />
                 <AccountProductServices productClass={productClass} />
